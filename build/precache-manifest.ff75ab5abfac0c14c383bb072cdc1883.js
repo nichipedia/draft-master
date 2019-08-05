9fef48151e4c981884852bceef3416999043f484
nichipedia <atreayou@gmail.com>
2019-08-04 22:32:12 -0500

    ugh!

---
+++ build/precache-manifest.ff75ab5abfac0c14c383bb072cdc1883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b129aa428fb4908e679f0de3966ca552",
+    "url": "./index.html"
+  },
+  {
+    "revision": "525aec634c7a244fd89d",
+    "url": "./static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "a1b7db8b7367ee9c718b",
+    "url": "./static/css/main.4575ae16.chunk.css"
+  },
+  {
+    "revision": "525aec634c7a244fd89d",
+    "url": "./static/js/2.9913ce58.chunk.js"
+  },
+  {
+    "revision": "a1b7db8b7367ee9c718b",
+    "url": "./static/js/main.094588d7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
